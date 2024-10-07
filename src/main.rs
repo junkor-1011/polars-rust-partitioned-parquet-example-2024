91ba4c0f4dee3689f33806d1635fa3685626534e
junkor-1011 <tomoyukizxy@gmail.com>
2024-10-07 23:30:59 +0900

    read csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use polars::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // read csv
+    let df = CsvReadOptions::default()
+        .map_parse_options(|parse_options| parse_options.with_try_parse_dates(true))
+        .try_into_reader_with_file_path(Some("data/input-csv/example.csv".into()))?
+        .finish()?;
+    println!("{df}"); // show content of DataFrame
+
+    Ok(())
 }
