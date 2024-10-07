674c94b02f94d93a7ca531a1aaffa9cb2ac25263
junkor-1011 <tomoyukizxy@gmail.com>
2024-10-08 00:46:29 +0900

    improve comment & format

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
 use polars::prelude::*;
 
+const INPUT_PATH: &str = "data/input-csv/example.csv";
+const OUTPUT_PATH: &str = "data/output/partitioned-example";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read csv
     let df = CsvReadOptions::default()
         .map_parse_options(|parse_options| parse_options.with_try_parse_dates(true))
-        .try_into_reader_with_file_path(Some("data/input-csv/example.csv".into()))?
+        .try_into_reader_with_file_path(Some(INPUT_PATH.into()))?
         .finish()?;
-    println!("{df}"); // show content of DataFrame
+    println!("DataFrame from csv: \n{df}\n"); // show content of DataFrame
 
     // write partitioned parquet
     {
         let mut df = df.clone();
         let _ = write_partitioned_dataset(
             &mut df,
-            std::path::Path::new("data/output/partitioned-example"),
+            std::path::Path::new(OUTPUT_PATH),
             vec!["type"],
             &ParquetWriteOptions::default(),
             4_294_967_296, // Ref: https://github.com/pola-rs/polars/blob/rs-0.43.1/py-polars/polars/dataframe/frame.py#L3651
         )?;
+        println!("success to write '{OUTPUT_PATH}'\n");
     }
 
     // read partitioned parquet
     {
         let lf = LazyFrame::scan_parquet_files(
-            vec!["data/output/partitioned-example".into()].into(),
+            vec![OUTPUT_PATH.into()].into(),
             ScanArgsParquet::default(),
         )?;
 
-        println!("DataFrame from partitioned parquet files: \n{}", lf.collect()?);
+        println!(
+            "DataFrame from partitioned parquet files: \n{}",
+            lf.collect()?
+        );
     }
 
     Ok(())
